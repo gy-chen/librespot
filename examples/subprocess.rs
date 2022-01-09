183379ef1131404aaa9f98a920bd4edce378f304
GYCHEN <gy.chen@gms.nutc.edu.tw>
2022-01-09 13:12:02 +0800

    add session reconnect

--- examples/subprocess.rs
+++ examples/subprocess.rs
@@ -1,40 +1,40 @@
 use std::env;
 use std::fs::File;
-use log::LevelFilter;
 
-use tokio::io::{AsyncBufReadExt, BufReader, stdin};
+use log::LevelFilter;
 use simplelog::{Config, WriteLogger};
+use tokio::io::{stdin, AsyncBufReadExt, BufReader};
 
 use librespot::core::config::SessionConfig;
 use librespot_core::authentication::Credentials;
-use librespot_core::session::Session;
+use librespot_core::session::{Session, SessionError};
 use librespot_core::spotify_id::SpotifyId;
 use librespot_playback::audio_backend;
+use librespot_playback::audio_backend::SinkBuilder;
 use librespot_playback::config::{AudioFormat, PlayerConfig};
 use librespot_playback::player::Player;
 
 #[tokio::main]
 async fn main() {
-    WriteLogger::init(LevelFilter::Debug, Config::default(), File::create("gyrespot.log").unwrap()).expect("Cannot setup write logger");
+    WriteLogger::init(
+        LevelFilter::Debug,
+        Config::default(),
+        File::create("gyrespot.log").unwrap(),
+    )
+    .expect("Cannot setup write logger");
 
-    let session_config = SessionConfig::default();
     let player_config = PlayerConfig::default();
     let audio_format = AudioFormat::default();
-
-    let username = env::var("USERNAME").expect("expect USERNAME env variable");
-    let password = env::var("PASSWORD").expect("expect PASSWORD env variable");
-
-    let credentials = Credentials::with_password(username, password);
-
     let backend = audio_backend::find(Some("pipe".into())).unwrap();
 
-    let session = Session::connect(session_config, credentials, None)
-        .await
-        .unwrap();
+    let mut session = connect_session().await.unwrap();
 
-    let (mut player, _) = Player::new(player_config, session, None, move || {
-        backend(None, audio_format)
-    });
+    let mut player = create_player(
+        player_config.clone(),
+        session.clone(),
+        audio_format.clone(),
+        backend.clone(),
+    );
 
     let stdin = stdin();
     let mut stdin = BufReader::new(stdin);
@@ -42,27 +42,57 @@ async fn main() {
     loop {
         let mut command = String::new();
 
-        stdin.read_line(&mut command)
-            .await
-            .unwrap();
+        stdin.read_line(&mut command).await.unwrap();
 
         let command = Command::parse(&command);
 
-        match command {
-            Ok(command) => {
-                match execute_command(&mut player, &command).await {
-                    Ok(_) => eprintln!("="),
-                    Err(msg) => eprintln!("?{}", msg)
-                }
+        loop {
+            if session.is_invalid() {
+                session = connect_session().await.unwrap();
+                player = create_player(
+                    player_config.clone(),
+                    session.clone(),
+                    audio_format.clone(),
+                    backend.clone(),
+                );
+                continue;
             }
-            Err(e) => eprintln!("?{}", e)
+            break;
+        }
+
+        match command {
+            Ok(command) => match execute_command(&mut player, &command).await {
+                Ok(_) => eprintln!("="),
+                Err(msg) => eprintln!("?{}", msg),
+            },
+            Err(e) => eprintln!("?{}", e),
         }
     }
 }
 
+fn create_player(
+    player_config: PlayerConfig,
+    session: Session,
+    audio_format: AudioFormat,
+    backend: SinkBuilder,
+) -> Player {
+    Player::new(player_config.clone(), session.clone(), None, move || {
+        backend(None, audio_format)
+    })
+    .0
+}
+
+async fn connect_session() -> Result<Session, SessionError> {
+    let session_config = SessionConfig::default();
+    let username = env::var("USERNAME").expect("expect USERNAME env variable");
+    let password = env::var("PASSWORD").expect("expect PASSWORD env variable");
+    let credentials = Credentials::with_password(username, password);
+    Session::connect(session_config, credentials, None).await
+}
+
 async fn execute_command(player: &mut Player, command: &Command) -> Result<(), String> {
     match command.type_ {
-        CommandType::PLAY => _play(player, command.track_id).await
+        CommandType::PLAY => _play(player, command.track_id).await,
     }
 }
 
@@ -73,7 +103,7 @@ async fn _play(player: &mut Player, track_id: SpotifyId) -> Result<(), String> {
 }
 
 enum CommandType {
-    PLAY
+    PLAY,
 }
 
 struct Command {
@@ -86,28 +116,22 @@ impl Command {
         let args: Vec<&str> = raw.split_whitespace().collect();
         match args.get(0) {
             Some(&"play") => match args.get(1) {
-                Some(&track_id) => {
-                    match SpotifyId::from_uri(&track_id) {
-                        Ok(track_id) => Ok(
-                            Command {
-                                type_: CommandType::PLAY,
-                                track_id,
-                            }
-                        ),
-                        Err(_) => match SpotifyId::from_base62(&track_id) {
-                            Ok(track_id) => Ok(
-                                Command {
-                                    type_: CommandType::PLAY,
-                                    track_id,
-                                }
-                            ),
-                            Err(_) => Err("cannot parse track_id:")
-                        }
-                    }
-                }
-                _ => Err("expect track_id parameter")
+                Some(&track_id) => match SpotifyId::from_uri(&track_id) {
+                    Ok(track_id) => Ok(Command {
+                        type_: CommandType::PLAY,
+                        track_id,
+                    }),
+                    Err(_) => match SpotifyId::from_base62(&track_id) {
+                        Ok(track_id) => Ok(Command {
+                            type_: CommandType::PLAY,
+                            track_id,
+                        }),
+                        Err(_) => Err("cannot parse track_id:"),
+                    },
+                },
+                _ => Err("expect track_id parameter"),
             },
-            _ => Err("unknown command")
+            _ => Err("unknown command"),
         }
     }
 }
